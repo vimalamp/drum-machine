06d1586f050124915128cab016f4054a0bf3bfdf
user <irchello@gmail.com>
2020-01-17 19:40:55 -1000

    Commiting the rest of files

---
+++ precache-manifest.c27d03923953f56f369cebc086d00157.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89caf582ebe49ba654ab345be84eb87d",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "d9953ea5adcdd5f4e2a5",
+    "url": "/drum-machine/static/css/2.79966c17.chunk.css"
+  },
+  {
+    "revision": "c57e326df8a5bc6be58d",
+    "url": "/drum-machine/static/css/main.af7be999.chunk.css"
+  },
+  {
+    "revision": "d9953ea5adcdd5f4e2a5",
+    "url": "/drum-machine/static/js/2.5d5c0707.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/drum-machine/static/js/2.5d5c0707.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c57e326df8a5bc6be58d",
+    "url": "/drum-machine/static/js/main.73f59218.chunk.js"
+  },
+  {
+    "revision": "307a70ee8ec5685596be",
+    "url": "/drum-machine/static/js/runtime-main.35e29f7d.js"
+  }
+]);
